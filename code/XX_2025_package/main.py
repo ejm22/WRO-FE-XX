5fe68b788d5a4d937048e783a8821b320fbc6185
Michael Bruneau <michaeldbruneau@gmail.com>
2025-08-11 20:32:09 -0400

    pi(edit): optimizing the obstacle challenge, we figured out which combinations were harder, still trying to add the proper corrections to the code. We also started the parking logic, but no progress so far

--- code/XX_2025_package/main.py
+++ code/XX_2025_package/main.py
@@ -21,7 +21,12 @@ if __name__ == "__main__":
     line=arduino.readline().decode().strip()
     if line:
         print(line)
-    i=0
+        level_text = line.replace("Battery voltage: ", "")
+        level_value = float(level_text)
+        if (level_value > 5) and (level_value < 11):
+            print("BATTERY LOW")
+            exit (1)
+
     camera_manager.capture_image()
     camera_manager.transform_image()
 
@@ -64,8 +69,7 @@ if __name__ == "__main__":
                     speed = 0
                 else:
                     break
-            i = i+1
-            print("index : ", i)
+
 
         ## 4 ##
         # Stop the robot in the correct zone
@@ -86,23 +90,25 @@ if __name__ == "__main__":
         while True:
             camera_manager.capture_image()
             camera_manager.transform_image()
-            
-            angle, image, is_green = ImageAlgorithms.find_obstacle_angle(camera_manager.obstacle_image.copy(), 
+            display_image = camera_manager.cropped_image.copy()
+            angle, display_image, is_green = ImageAlgorithms.find_obstacle_angle(camera_manager.obstacle_image.copy(), 
                                                                camera_manager.hsv_image.copy(), 
-                                                               camera_manager.cropped_image.copy(), 
+                                                               display_image, 
                                                                camera_manager.grayscale_image.copy())
+            if display_image is not None:
+                cv2.imshow("Display_image", display_image)
             print("Angle objet : ", angle)
             angle_walls = ImageAlgorithms.calculate_servo_angle_walls(camera_manager.polygon_image)
             angle_obstacles = ImageAlgorithms.calculate_servo_angle_obstacle(angle, is_green)
             servo_angle = ImageAlgorithms.choose_output_angle(angle_walls, angle_obstacles)
-            print(servo_angle)
+            print("angle_walls,angle_obstacles, servo_angle", angle_walls, angle_obstacles, servo_angle)
 
             #cv2.imshow("Polygon image", camera_manager.polygon_image)
             #cv2.imshow("Cropped image", camera_manager.cropped_image)
             #cv2.imshow("Lol", camera_manager.obstacle_image)
+            cv2.imshow("Binary image", camera_manager.binary_image)
             cv2.imshow("Obstacle with rect", camera_manager.contour_obstacle_with_rect)
-            cv2.imshow("Rectangle contour", camera_manager.contour_obstacle_with_rect)
-
+            #cv2.imshow("Pink image", camera_manager.pink_image)
             command = f"{servo_angle},{speed}.".encode()
             arduino.write(command)
             arduino.flush()
@@ -115,6 +121,46 @@ if __name__ == "__main__":
 
         ## 5 ##
         # Parallel park in the parking area
+
+    if (defi == 3):
+       
+        print("Defi 3")
+        speed = 500
+        while True:
+            arduino.flushInput()
+            camera_manager.capture_image()
+            camera_manager.transform_image()
+
+            cv2.imshow("Cropped", camera_manager.cropped_image)
+            cv2.imshow("Polygon Image", camera_manager.polygon_image)
+            #print("Poly Lines = ", camera_manager.polygon_lines)
+            angle_walls = ImageAlgorithms.calculate_servo_angle_walls(camera_manager.polygon_image)
+            print ("angle_walls = ", angle_walls)
+            wall_angle = ImageAlgorithms.get_facing_wall_angle(camera_manager.polygon_lines)
+            print("Exterior wall angle = ", wall_angle)
+
+            angle = ImageAlgorithms.calculate_servo_angle_walls(camera_manager.polygon_image)
+            servo_angle = ImageAlgorithms.calculate_servo_angle_parking(angle_walls, wall_angle)
+
+            #command = f"{servo_angle},{speed}.".encode()
+            if camera_manager.binary_image[63, 320] == 0:
+                speed = 0
+            else:
+                speed = 300
+            command = f"{servo_angle},{speed}.".encode()
+            
+
+            arduino.write(command)
+            arduino.flush()
+        
+            time.sleep(0.01)
+            key = cv2.waitKey(1)  # Let OpenCV update the window
+            if key == 27:  # Escape key to quit
+                if (speed != 0):
+                    speed = 0
+                else:
+                    break
+           
         
     cv2.destroyAllWindows()
     arduino.write(b'88,0.')
