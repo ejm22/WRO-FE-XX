0f5e4dc8d0b660d4eba0c360252b15775a93eebf
Michael Bruneau <michaeldbruneau@gmail.com>
2025-09-27 21:05:08 -0400

    pi(new) : added arduino_comms class and update main

---
+++ code/XX_2025_package/classes/arduino_comms.py
@@ -0,0 +1,25 @@
+import serial
+import time
+
+class ArduinoComms:
+	def __init__(self, port='/dev/ttyACM0', baudrate=115200, timeout=0.1):
+		self.arduino = serial.Serial(port, baudrate, timeout=timeout)
+
+	def send(self, type, angle = 85, speed = 0, distance = 0):
+		if type == '!':
+			command = f"{angle}!".encode()
+			self.arduino.write(command)
+		elif type == 'm':
+			command = f"{angle},{speed}".encode()
+			self.arduino.write(command)
+		elif type == 't':
+			command = f"{angle},{speed},{distance}".encode()
+			self.arduino.write(command)
+			while (self.read() != 'F'):
+				time.sleep(0.005)
+		self.arduino.flush()
+
+	def read(self, size=1):
+		if self.arduino.in_waiting > 0:
+			return self.arduino.read(size).decode('utf-8')
+		return None
--- code/XX_2025_package/main.py
+++ code/XX_2025_package/main.py
@@ -2,20 +2,18 @@ import serial
 import cv2
 import time
 import numpy as np
-from XX_2025_package.classes.camera_manager import CameraManager
-from XX_2025_package.utils.image_transform_utils import ImageTransformUtils
-from XX_2025_package.classes.image_algoriths import ImageAlgorithms
-from XX_2025_package.classes.context_manager import ContextManager
-from XX_2025_package.classes.lap_tracker import LapTracker
-from XX_2025_package.utils.enums import Direction
-from XX_2025_package.utils.enums import StartPosition
-from XX_2025_package.utils.image_drawing_utils import ImageDrawingUtils
+from classes.camera_manager import CameraManager
+from utils.image_transform_utils import ImageTransformUtils
+from classes.image_algoriths import ImageAlgorithms
+from classes.context_manager import ContextManager
+from classes.lap_tracker import LapTracker
+from utils.enums import Direction
+from utils.enums import StartPosition
+from utils.image_drawing_utils import ImageDrawingUtils
+from classes.arduino_comms import ArduinoComms
 
 MOVE_TO_FRONT_ZONE = 3200
 stop_run = False
-allow_stop_run = False
-
-arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
 
 # 0.1559 mm per 1 step
 # 1 mm per 6.412 steps
@@ -28,37 +26,33 @@ if __name__ == "__main__":
         context_manager = ContextManager()
         lap_tracker = LapTracker(context_manager)
         image_algorithms = ImageAlgorithms(context_manager, camera_manager)
+        arduino = ArduinoComms()
         camera_manager.start_camera()
-        command = f"10000000!".encode()
-        arduino.write(command)
-        arduino.flush()
+        arduino.send('!', 10000000)
 
         ## 1 ##
         # Wait for start button to be pressed
         while True:
-            if arduino.in_waiting > 0:
-                data = arduino.read().decode('utf-8')
-                if data == '1':
+            msg = arduino.read()
+            if msg:
+                if msg == '1':
                     context_manager.set_challenge(1)
                     print("Challenge 1 selected")
                     break
                     
-                elif data == '2':
+                elif msg == '2':
                     context_manager.set_challenge(2)
                     print("Challenge 2 selected")
                     break
 
-                elif data == '0':
-                    print("0")
-                    # only check after at least one run, so program isnt accidentally stopped before first attempt
-                    if (allow_stop_run):
-                        stop_run = True
-                        break
+                elif msg == '0':
+                    print("Leave program")
+                    stop_run = True
+                    break
                 time.sleep(0.005)
 
         if stop_run:
             break
-        allow_stop_run = True
         time.sleep(1)
         camera_manager.capture_image()
         camera_manager.transform_image()
@@ -87,9 +81,7 @@ if __name__ == "__main__":
                 camera_manager.transform_image()
                 lap_tracker.process_image(camera_manager.cnt_blueline, camera_manager.cnt_orangeline)
                 angle, is_corner = image_algorithms.calculate_servo_angle_from_walls()
-                command = f"m{angle},{speed}.".encode()
-                arduino.write(command)
-                arduino.flush()
+                arduino.send('m', angle, speed)
                 ImageDrawingUtils.add_text_to_image(camera_manager.display_image, f"Lap: {context_manager.get_lap_count()}", (10, 30), (0, 0, 255))
                 camera_manager.add_frame_to_video()
                 cv2.imshow("Display", camera_manager.display_image)
@@ -111,18 +103,13 @@ if __name__ == "__main__":
                             extra_move = 0
                         # Adjust move based on final corner position
                         if final_corner_position == 'C':
-                            command = f"{6400 + extra_move}!".encode()
-                            arduino.write(command)
+                            arduino.send('!', 6400 + extra_move)
                             speed = 1500
                         else:
-                            command = f"{3900 + extra_move}!".encode()
-                            arduino.write(command)
+                            arduino.send('!', 3900 + extra_move)
                             speed = 1500
-                if arduino.in_waiting > 0:
-                    if context_manager.has_completed_laps():
-                        if arduino.read().decode('utf-8') == 'F':
-                            break
-                        time.sleep(0.005)
+                if context_manager.has_completed_laps() and arduino.read() == 'F':
+                    break
                 # Let OpenCV update the window
                 key = cv2.waitKey(1)  
                 # Press escape key to quit (when testing)
@@ -137,7 +124,7 @@ if __name__ == "__main__":
         ################################################################
         # This comment is to test a git push bug we're facing
         if (ContextManager.CHALLENGE == 2):
-            arduino.write(b"10000000!")
+            arduino.send('!', 10000000)
             speed = 3000
             
             ## 1 ##
@@ -148,23 +135,16 @@ if __name__ == "__main__":
             # Analyze starting area (optional)
             # Leave parking spot
             if context_manager.get_direction() == Direction.LEFT:
-                command = f"t120,{speed},1200.".encode()
-                arduino.write(command)
+                arduino.send('t', 120, speed, 1200)
             else:
                 print("Move right")
-                command = f"t50,{speed},1200.".encode()
-                arduino.write(command)
-            print("Wait to complete")
-            while arduino.read().decode('utf-8') != 'F':
-                time.sleep(0.005)
-            command = f"t85,{speed},1000.".encode()
-            arduino.write(command)
-            while arduino.read().decode('utf-8') != 'F':
-                time.sleep(0.005)
-            print("Complete")
+                arduino.send('t', 50, speed, 1200)
+            
+            arduino.send('t', 85, speed, 1000)
+        
             ## 3 ##
             # Complete 3 laps
-            arduino.write(b"10000000!")
+            arduino.send('!', 10000000)
             while True:
                 camera_manager.capture_image()
                 camera_manager.transform_image()
@@ -178,10 +158,8 @@ if __name__ == "__main__":
                 angle_obstacles = image_algorithms.calculate_servo_angle_from_obstacle(angle, is_green)
                 servo_angle = image_algorithms.choose_output_angle(angle_walls, angle_obstacles)
 
-                command = f"m{servo_angle},{speed}.".encode()
-                arduino.write(command)
-                arduino.flush()
-                
+                arduino.send('m', servo_angle, speed)
+
                 camera_manager.add_frame_to_video()
 
                 cv2.imshow("Polygon Image", camera_manager.polygon_image)
@@ -212,7 +190,7 @@ if __name__ == "__main__":
         ################################################################
         
         if (ContextManager.CHALLENGE == 2):
-            arduino.write(b"10000000!")
+            arduino.send('!', 10000000)
             print("Going to parking")
             speed = 2000
             ## 1 ##
@@ -253,10 +231,7 @@ if __name__ == "__main__":
                 angle_walls, _ = image_algorithms.calculate_servo_angle_from_walls()
                 servo_angle = image_algorithms.choose_output_angle(angle_walls, angle_obstacles)
                 ImageDrawingUtils.draw_circle(camera_manager.display_image, (pink_pixel_x, pink_pixel_y), 5, (255, 0, 255))
-                command = f"m{servo_angle},{speed}.".encode()
-                arduino.write(command)
-                arduino.flush()
-                
+                arduino.send('m', servo_angle, speed)
                 camera_manager.add_frame_to_video()
                 if pink_x is not None and pink_y is not None:
                     if (context_manager.get_direction() == Direction.RIGHT and pink_x < 150 and pink_y > ImageTransformUtils.PIC_HEIGHT - 100) or (context_manager.get_direction() == Direction.LEFT and pink_x > ImageTransformUtils.PIC_WIDTH - 150 and pink_y > ImageTransformUtils.PIC_HEIGHT - 100):
@@ -264,14 +239,13 @@ if __name__ == "__main__":
                 key = cv2.waitKey(1)  # Let OpenCV update the window
                 if key == 27:  # Escape key to quit
                     break
-            command = f"m85,0.".encode()
-            arduino.write(command)
+            arduino.send('m', 85, 0)
 
             ## 5 ##
             # Parallel park in the parking area
             print("Challenge 3")
             speed = 1000
-            arduino.write(b"2000!")
+            arduino.send('!', 2000)
             arduino_flag = False
             if context_manager.get_direction() == Direction.LEFT:
                 context_manager.set_direction(Direction.RIGHT)
@@ -288,23 +262,18 @@ if __name__ == "__main__":
                 #print ("angle_walls = ", angle_walls)
                 top_angle = image_algorithms.get_top_line_angle()
                 #print("Top angle = ", top_angle)
-                if arduino.in_waiting > 0:
-                    if arduino.read().decode('utf-8') == 'F':
-                        print("Done")
-                        arduino.write(b"10000000!")
-                        arduino_flag = True
+                if arduino.read() == 'F':
+                    print("Done")
+                    arduino.send('!', 10000000)
+                    arduino_flag = True
                 if (arduino_flag and camera_manager.binary_image[140, ImageTransformUtils.PIC_WIDTH // 2] == 0 and top_angle is not None and context_manager.get_direction() == Direction.RIGHT) or (arduino_flag and np.any(camera_manager.cnt_orangeline[ImageTransformUtils.PIC_HEIGHT - 100: ImageTransformUtils.PIC_HEIGHT - 65, ImageTransformUtils.PIC_WIDTH // 2 - 20: ImageTransformUtils.PIC_WIDTH // 2 + 20]) and context_manager.get_direction() == Direction.LEFT):
                     speed = 0
-                    command = f"m85,0.".encode()
-                    arduino.write(command)
+                    arduino.send('m', 85, 0)
                     break
                 #else:
                 #    speed = 1000
-                command = f"m{angle_walls},{speed}.".encode()
-                
+                arduino.send('m', angle_walls, speed)
                 camera_manager.add_frame_to_video()
-                arduino.write(command)
-                arduino.flush()
             
                 time.sleep(0.01)
                 key = cv2.waitKey(1)  # Let OpenCV update the window
@@ -314,7 +283,7 @@ if __name__ == "__main__":
                     else:
                         break
 
-            arduino.write(b"-10000000!")
+            arduino.send('!', 10000000)
             # Back up until pink wall is detected
             while True:
                 camera_manager.capture_image()
@@ -322,56 +291,37 @@ if __name__ == "__main__":
                 cv2.imshow("Cropped", camera_manager.cropped_image)
                 cv2.imshow("Polygon Image", camera_manager.polygon_image)
                 if camera_manager.binary_image[pink_pixel_backwards_y, pink_pixel_backwards_x] == 0:
-                    command = f"m85,0.".encode()
-                    arduino.write(command)
+                    arduino.send('m', 85, 0)
                     break
-                command = f"m85,-1000.".encode()
-                arduino.write(command)
+                arduino.send('m', 85, -1000)
             time.sleep(0.2)
 
             #speed = 1000
-            # Move forward a bit to prepare to enter the parking spot            
-            command = f"t85,1000,{1700}.".encode()
-            arduino.write(command)
-            while arduino.read().decode('utf-8') != 'F':
-                time.sleep(0.005)
+            # Move forward a bit to prepare to enter the parking spot
+            arduino.send('t', 85, 1000, 1700)
+            
             print(context_manager.get_direction())
             # Enter the parking spot while turning
             if (context_manager.get_direction() == Direction.LEFT and last_color == 1) or (context_manager.get_direction() == Direction.RIGHT and last_color == 0):
                 print("Too close, turn less")
-                command = f"t{87 - context_manager.get_direction().value * 37},-1000,1300.".encode()
+                arduino.send('t', 87 - context_manager.get_direction().value * 37, -1000, 1300)
             else:
                 print("Normal")
-                command = f"t{87 - context_manager.get_direction().value * 37},-1000,1400.".encode()
-            arduino.write(command)
-            while arduino.read().decode('utf-8') != 'F':
-                time.sleep(0.005)
+                arduino.send('t', 87 - context_manager.get_direction().value * 37, -1000, 1400)
+            
             # Backup straight inside the parking spot
-            command = f"t85,-1000,650.".encode()
-            arduino.write(command)
-            while arduino.read().decode('utf-8') != 'F':
-                time.sleep(0.005)
+            arduino.send('t', 85, -1000, 650)
             # Backup while turning to straighten the robot
-            command = f"t{87 + context_manager.get_direction().value * 37},-1000,1225.".encode()
-            arduino.write(command)
-            while arduino.read().decode('utf-8') != 'F':
-                time.sleep(0.005)
+            arduino.send('t', 87 + context_manager.get_direction().value * 37, -1000, 1225)
             # Straighten the wheels
-            command = f"m85,0.".encode()
-            arduino.write(command)
+            arduino.send('m', 85, 0)
         
         ################################################################
         #################### Code for movement tests ###################
         ################################################################
         if (ContextManager.CHALLENGE == 5):
             while True:
-                command = f"t85,1000,10000.".encode()
-                arduino.write(command)
-                while arduino.read().decode('utf-8') != 'F':
-                    time.sleep(0.005)
-                command = f"t85,-1000,10000.".encode()
-                arduino.write(command)
-                while arduino.read().decode('utf-8') != 'F':
-                    time.sleep(0.005)
+                arduino.send('t', 85, 1000, 10000)
+                arduino.send('t', 85, -1000, 10000)
         cv2.destroyAllWindows()
-        arduino.write(b'm85,0.')
+        arduino.send('m', 85, 0)
